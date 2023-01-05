024ea6f701709cdb9365523ab1d3afa9fa9eb864
Steven Zuluaga <steven000069@hotmail.com>
2023-01-05 11:04:38 -0500

    Iniciando proyecto

---
+++ components/SignUp.tsx
@@ -0,0 +1,171 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import StarIcon from '@mui/icons-material/StarBorder';
+import Typography from '@mui/material/Typography';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+
+ 
+  const tiers = [
+    
+    {
+      title: 'Inscripción Segunda Cohorte',
+      description: [
+        'Formación en habilidades digitales para el empleo'
+        
+      ],
+      buttonText: 'Inscribete',
+      buttonVariant: 'outlined',
+      urlSignUp:'https://docs.google.com/forms/d/e/1FAIpQLSd6updAL1JRnu6by8_liQOIOxJUAxxbXSuMGuDqJepKFUp64g/viewform?usp=sharing'
+    },
+  ];
+  /* const footers = [
+    {
+      title: 'Company',
+      description: ['Team', 'History', 'Contact us', 'Locations'],
+    },
+    {
+      title: 'Features',
+      description: [
+        'Cool stuff',
+        'Random feature',
+        'Team feature',
+        'Developer stuff',
+        'Another one',
+      ],
+    },
+    {
+      title: 'Resources',
+      description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    },
+    {
+      title: 'Legal',
+      description: ['Privacy policy', 'Terms of use'],
+    },
+  ]; */
+  
+const SignUp = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      
+      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Código C13
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" component="p">
+        El talento no tiene estrato social.
+        </Typography>
+      </Container>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} direction="column"
+  alignItems="center"
+  justifyContent="center">
+          {tiers.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === 'Enterprise' ? 12 : 6}
+              md={4}
+            >
+              <Card>
+                <CardHeader
+                  title={tier.title}
+                  
+                  titleTypographyProps={{ align: 'center' }}
+                  action={ <StarIcon /> }
+                  subheaderTypographyProps={{
+                    align: 'center',
+                  }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  
+                  <ul>
+                    {tier.description.map((line) => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
+                        {line}
+                      </Typography>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardActions>
+                <Button
+                    fullWidth
+                    variant='contained'
+                    href={tier.urlSignUp}
+                    target="_blank"
+                  >
+                    {tier.buttonText}
+                  </Button>
+               
+                  
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      {/* Footer */}
+      <Container
+        maxWidth="md"
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+          mt: 8,
+          py: [3, 6],
+        }}
+      >
+       {/*  <Grid container spacing={4} justifyContent="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="text.primary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="text.secondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid> */}
+        
+      </Container>
+      {/* End footer */}
+    </React.Fragment>
+  )
+}
+
+export default SignUp
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUp from '../components/SignUp'
+
+const index = () => {
+  return (
+    <SignUp/>
+  )
+}
+
+export default index
